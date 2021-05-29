a7a15c51f64692b1ac78d3bc0e6ec1d630b1b880
Goctionni <Goctionni@gmail.com>
2021-05-29 22:09:01 +0200

    You can now move around the map and it kinda works

--- src/components/Viewer/Viewer.vue
+++ src/components/Viewer/Viewer.vue
@@ -2,7 +2,7 @@
   <div class="viewer" v-if="show">
     <div class="viewport">
       <div class="tilt">
-        <div class="player" :class="`facing-${facing}`">
+        <div class="player" :class="`facing-${facing}`" :style="`transform: rotateZ(${angle}deg);`">
           <div class="map">
             <div class="row" v-for="(row, r) in viewMap" :key="r">
               <div
@@ -17,6 +17,11 @@
         </div>
       </div>
     </div>
+    <div class="controls">
+      <button class="turn-left" @click="rotateLeft()"></button>
+      <button class="go-forwards" @click="goForwards()" :disabled="!canMoveForwards"></button>
+      <button class="turn-right" @click="rotateRight()"></button>
+    </div>
   </div>
 </template>
 
@@ -26,12 +31,14 @@ import { Component, Vue, Prop, Watch } from "vue-property-decorator";
 import { createEmptyTile } from '@/util';
 
 type Facing = 'north' | 'east' | 'south' | 'west';
+const directions: Facing[] = ['north', 'east', 'south', 'west'];
 
 @Component
 export default class Sidebar extends Vue {
   @Prop() map!: Map;
   @Prop() show!: boolean;
   facing: Facing = 'north';
+  angle = 0;
   x = 0;
   y = 0;
 
@@ -48,7 +55,7 @@ export default class Sidebar extends Vue {
             else if (tile.y === y) return { ...tile, south: '' };
             else return { ...tile };
           case 'east':
-            if (tile.x > x) return createEmptyTile(tile.x, tile.y);
+            if (tile.x < x) return createEmptyTile(tile.x, tile.y);
             else if (tile.x === x) return { ...tile, west: '' };
             else return { ...tile };
           case 'south':
@@ -56,7 +63,7 @@ export default class Sidebar extends Vue {
             else if (tile.y === y) return { ...tile, north: '' };
             else return { ...tile };
           case 'west':
-            if (tile.x < x) return createEmptyTile(tile.x, tile.y);
+            if (tile.x > x) return createEmptyTile(tile.x, tile.y);
             else if (tile.x === x) return { ...tile, east: '' };
             else return { ...tile };
           default:
@@ -74,12 +81,17 @@ export default class Sidebar extends Vue {
     return this.y;
   }
 
+  get canMoveForwards(): boolean {
+    const facing = this?.facing || 'north';
+    const rows = this.map?.rows || [];
+    const { x, y } = this;
+    const tile = rows[y][x];
+    return !tile[facing];
+  }
+
   mounted(): void {
     this.updateCSSVariables();
     Object.assign(window, { viewer: this });
-    setInterval(() => {
-      this.rotateCW();
-    }, 3000);
   }
 
   @Watch('map.startTile', { immediate: true, deep: true })
@@ -97,19 +109,32 @@ export default class Sidebar extends Vue {
     (this.$el as HTMLElement).style.setProperty('--viewportYOffset', this.viewYOffset.toString());
   }
 
-  rotateCW(): void {
+  rotateRight(): void {
+    const nextFacingIndex = (directions.indexOf(this.facing) + 1) % directions.length;
+    this.facing = directions[nextFacingIndex];
+    this.angle -= 90;
+  }
+
+  rotateLeft(): void {
+    const currentFacingIndex = directions.indexOf(this.facing);
+    const nextFacingIndex = currentFacingIndex === 0 ? directions.length - 1 : currentFacingIndex - 1;
+    this.facing = directions[nextFacingIndex];
+    this.angle += 90;
+  }
+
+  goForwards(): void {
     switch(this.facing) {
       case 'north':
-        this.facing = 'east';
+        this.y--;
         break;
       case 'east':
-        this.facing = 'south';
+        this.x++;
         break;
       case 'south':
-        this.facing = 'west';
+        this.y++;
         break;
       case 'west':
-        this.facing = 'north';
+        this.x--;
         break;
     }
   }
@@ -119,16 +144,23 @@ export default class Sidebar extends Vue {
 <style lang="scss" scoped>
 @import '../../faces';
 
-.viewport {
+.viewer {
   --viewportSize: 800px;
   position: fixed;
+  display: flex;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  width: calc(1.5 * var(--viewportSize));
+  gap: 10px;
+}
+
+.viewport {
   overflow: hidden;
   width: 800px;
   height: 800px;
   outline: solid #000 1px;
+  outline-offset: -1px;
   transform-style: preserve-3d;
   perspective: calc(1.5 * var(--viewportSize));
 }
@@ -147,9 +179,9 @@ export default class Sidebar extends Vue {
   }
 }
 .player.facing-east {
-  transform: rotateZ(90deg);
+  transform: rotateZ(270deg);
   .map {
-    left: calc(-1 * (var(--viewportXOffset) - 0.5) * var(--viewportSize));
+    left: calc(-1 * (var(--viewportXOffset) + 0.5) * var(--viewportSize));
   }
 }
 .player.facing-south {
@@ -159,19 +191,21 @@ export default class Sidebar extends Vue {
   }
 }
 .player.facing-west {
-  transform: rotateZ(270deg);
+  transform: rotateZ(90deg);
   .map {
-    left: calc(-1 * (var(--viewportXOffset) + 0.5) * var(--viewportSize));
+    left: calc(-1 * (var(--viewportXOffset) - 0.5) * var(--viewportSize));
   }
 }
 
 .player, .tilt {
   width: var(--viewportSize);
   height: var(--viewportSize);
+  transform-style: preserve-3d;
 }
 
 .player {
   transition: transform ease-in-out .5s;
+  transform-style: preserve-3d;
 }
 
 .tilt {
@@ -194,5 +228,103 @@ export default class Sidebar extends Vue {
   max-width: var(--viewportSize);
   max-height: var(--viewportSize);
   position: relative;
+}
+
+.controls {
+  display: flex;
+  width: fit-content;
+  gap: 10px;
+
+  > button {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100px;
+    height: 100px;
+    overflow: hidden;
+    cursor: pointer;
+    border-radius: 5px;;
+    border: 0;
+    background: #999;
+
+    &:hover {
+      background-color: #CCC;
+    }
+
+    &:disabled {
+      opacity: .5;
+      pointer-events: none;
+    }
+
+    &::before,
+    &::after {
+        content: '';
+        position: absolute;
+        display: block;
+    }
+
+    &.turn-left {
+      &::before {
+        width: 125%;
+        height: 125%;
+        border: solid #000 6px;
+        border-radius: 50%;
+        top: 20%;
+        left: -50%;
+        clip-path: polygon(50% 0%, 100% 0%, 100% 50%, 50% 50%);
+      }
+
+      &::after {
+        left: 10px;
+        top: 9px;
+        width: 20px;
+        height: 30px;
+        background-color: #000;
+        clip-path: polygon(0% 50%, 100% 0%, 100% 100%);
+      }
+    }
+
+    &.turn-right {
+      &::before {
+        width: 125%;
+        height: 125%;
+        border: solid #000 6px;
+        border-radius: 50%;
+        top: 20%;
+        right: -50%;
+        clip-path: polygon(50% 0%, 0% 0%, 0% 50%, 50% 50%);
+      }
+
+      &::after {
+        right: 10px;
+        top: 9px;
+        width: 20px;
+        height: 30px;
+        background-color: #000;
+        clip-path: polygon(0% 0%, 100% 50%, 0% 100%);
+      }
+    }
+
+    &.go-forwards {
+      &::before {
+        left: 50%;
+        width: 6px;
+        top: 15%;
+        bottom: 10%;
+        margin-left: -3px;
+        background-color: #000;
+      }
+      &::after {
+        height: 20px;
+        background-color: #000;
+        left: 30%;
+        right: 30%;
+        top: 10%;
+        transform-origin: top left;
+        clip-path: polygon(50% 0%, 100% 100%, 0% 100%);
+      }
+    }
+  }
 }
 </style>
